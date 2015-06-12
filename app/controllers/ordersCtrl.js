a0f096456aeb29893cf5fad17b52523bed7d8747
naveed <naveedjb@hotmail.com>
2015-06-13 01:47:43 +0500

    adding Orders and Products Controller

--- app/controllers/ordersCtrl.js
+++ app/controllers/ordersCtrl.js
@@ -1,3 +1,85 @@
 /**
  * Created by naveed on 6/10/2015.
  */
+var mongoose=require('mongoose'),
+    Order=mongoose.model('Order'),
+    Customer=mongoose.model('Customer'),
+    Address=mongoose.model('Address'),
+    Billing=mongoose.model('Billing');
+
+
+exports.getOrder=function(req,res){
+
+    Order.findOne(function(err,order){
+    if(!order)
+    {
+
+    res.json(404,{msg:'No Order Found'})
+    }
+    else{
+
+        res.json(order);
+    }
+
+    })
+
+
+
+};
+
+exports.getOrders=function(req,res){
+
+    Order.find(function(err,order){
+        if(!order)
+        {
+
+            res.json(404,{msg:'No Order Found'})
+        }
+        else{
+
+            res.json(order);
+        }
+
+    })
+
+
+
+};
+
+exports.addOrder=function(re,res){
+
+    var orderShipping= new Address(req.body.updatedShipping);
+    var orderBilling=  new Billing (req.body.updatedBilling);
+    var orderItems= req.body.orderItems;
+    var newOrder= new Order({userid:'CustomerA', items:orderItems, shipping: orderShipping, billing: orderBilling})
+    newOrder.save(function(err,results){
+
+        if(err)
+        {
+
+            res.json(500,{msg:'Failed to save Order'})
+        }
+        else{
+
+           Customer.update({userid:'customerA'},{$set:{cart:[]}}).
+               exec(function(err,results){
+                   if(err || results<1){
+
+                       res.json(404,{msg: 'Failed to update Cart'})
+                   }
+                   else {
+                       res.json({msg: 'Order Saved'})
+
+                   }
+
+
+               })
+        }
+
+
+    });
+
+
+
+
+};
--- app/controllers/productsCtrl.js
+++ app/controllers/productsCtrl.js
@@ -1,3 +1,46 @@
 /**
  * Created by naveed on 6/10/2015.
  */
+var mongoose=require('mongoose'),
+    Product=mongoose.model('Product');
+
+exports.getProduct=function(req, res){
+Product.findone({_id:req.query.productId}).exec(function(err,product){
+    if(!product)
+    {
+        res.json(404,{msg:'Photo Not Found'});
+    }
+    else
+    {
+        res.json(product);
+    }
+
+
+})
+
+
+
+
+};
+
+exports.getProducts=function(req,res){
+
+    Product.find().exec(function(err,products){
+
+        if(!products){
+
+            res.json(404,{msg:"photos not found"})
+        }
+        else{
+
+            res.json(products);
+        }
+
+
+
+    })
+
+
+
+
+}
