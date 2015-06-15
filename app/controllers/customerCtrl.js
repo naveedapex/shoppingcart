01032185d33e6a2ae11d94efa9d00d422ac88bf9
naveed <naveedjb@hotmail.com>
2015-06-16 01:28:56 +0500

    add customer ctrl

--- app/controllers/customerCtrl.js
+++ app/controllers/customerCtrl.js
@@ -1,3 +1,78 @@
-/**
- * Created by naveed on 6/10/2015.
- */
+
+var mongoose=require('mongoose'),
+    Order=mongoose.model('Order'),
+    Customer=mongoose.model('Customer'),
+    Address=mongoose.model('Address'),
+    Billing=mongoose.model('Billing');
+
+exports.getCustomer=function(req, res){
+    Customer.findOne({userid:'customerA'}).
+        exec(function(err,customer){
+            if(!customer){
+                res.json(404,{msg: 'Customer not found'});
+            }
+            else
+            {
+                res.json(customer);
+
+            }
+        })
+
+}
+
+
+exports.updateShipping=function(req,res){
+    var newShipping=new Address(req.body.updateShipping);
+
+    Customer.update({userid:'customerA'},{$set:[newShipping.toObject()]})
+        .exec(function(err,results){
+            if(err || results < 1){
+                res.json(404,{msg: 'Failed to update Shipping'})
+            }
+            else
+            {
+                res.json({msg:'Customer Shipping updated'})
+
+            }
+
+
+        })
+}
+
+exports.updateBilling=function(req,res){
+
+    var newBilling=new Billing(req.body.updatedBilling);
+
+    Customer.update({userid:'customerA'},{$set:{billing:[newBilling.toObject()]}})
+        .exec(function(err, results){
+            if(err || results<1){
+
+                res.json(404,{msg: 'failed to update Billing'});
+            }
+            else
+            {
+                res.json({msg: 'Customer Billing Updated'});
+            }
+
+       })
+}
+
+exports.updateCart=function(req,res){
+    Customer.update({userid:'customerA'},{$set:{cart:req.body.updatedCart}})
+        .exec(function(err,results){
+            if(err || results<1){
+
+                res.json(404,{msg:'Failed to update cart.'})
+
+
+            }
+            else
+            {
+                res.json({msg:'Failed to update cart.'})
+
+            }
+
+        })
+
+
+}
