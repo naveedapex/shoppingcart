ced6696a538da37ff4f3de5e8b15e79a7ed7ac71
naveed <naveedjb@hotmail.com>
2015-06-19 00:54:35 +0500

    add cartCtrl contents

--- public/cart/cart.js
+++ public/cart/cart.js
@@ -9,6 +9,37 @@ angular.module('cart', ['ngRoute'])
   });
 }])
 
-.controller('CartCtrl', [function() {
-    
+.controller('CartCtrl', ['$scope',function($scope) {
+
+      $scope.months=[1,2,3,4,5,6,7,8,9,10,11,12];
+      $scope.years=[2014,2015,2016,2017,2018,2019,2020];
+      $http.get('/products')
+          .success(function(data,status,headers,config){
+            $scope.products=data;
+            $scope.product=data[0];
+          })
+          .error(function(data,status,headers,config){
+            $scope.products=[];
+
+          })
+      $http.get('/customers')
+          .success(function(data,status,headers,config){
+            $scope.customer=data;
+
+          })
+          .error(function(data,status,headers,config){
+            $scope.customer=[];
+
+          })
+
+      $http.get('/orders')
+          .success(function(data,status,headers,config){
+            $scope.orders=data;
+
+          })
+          .error(function(data,status,headers,config){
+            $scope.orders=[];
+
+          })
+
 }]);
